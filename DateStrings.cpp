35713d13d1cce604b27684d47026e6ca76fb38ff
Alnoa <info@alnoa.fr>
2015-01-21 16:45:13 +0100

    traduction des chaines jours et mois

--- DateStrings.cpp
+++ DateStrings.cpp
@@ -7,6 +7,7 @@
  * You can change the text of the strings, make sure the short strings are each exactly 3 characters 
  * the long strings can be any length up to the constant dt_MAX_STRING_LEN defined in Time.h
  * 
+ *  French translation by Alnoa , www.alnoa.fr
  */
 
 #if defined(__AVR__)
@@ -28,18 +29,18 @@
 static char buffer[dt_MAX_STRING_LEN+1];  // must be big enough for longest string and the terminating null
 
 const char monthStr0[] PROGMEM = "";
-const char monthStr1[] PROGMEM = "January";
-const char monthStr2[] PROGMEM = "February";
-const char monthStr3[] PROGMEM = "March";
-const char monthStr4[] PROGMEM = "April";
-const char monthStr5[] PROGMEM = "May";
-const char monthStr6[] PROGMEM = "June";
-const char monthStr7[] PROGMEM = "July";
-const char monthStr8[] PROGMEM = "August";
-const char monthStr9[] PROGMEM = "September";
-const char monthStr10[] PROGMEM = "October";
-const char monthStr11[] PROGMEM = "November";
-const char monthStr12[] PROGMEM = "December";
+const char monthStr1[] PROGMEM = "Janvier";
+const char monthStr2[] PROGMEM = "Fevrier";
+const char monthStr3[] PROGMEM = "Mars";
+const char monthStr4[] PROGMEM = "Avril";
+const char monthStr5[] PROGMEM = "Mai";
+const char monthStr6[] PROGMEM = "Juin";
+const char monthStr7[] PROGMEM = "Juillet";
+const char monthStr8[] PROGMEM = "Aout";
+const char monthStr9[] PROGMEM = "Septembre";
+const char monthStr10[] PROGMEM = "Octobre";
+const char monthStr11[] PROGMEM = "Novembre";
+const char monthStr12[] PROGMEM = "Decembre";
 
 const PROGMEM char * const PROGMEM monthNames_P[] =
 {
@@ -47,23 +48,23 @@ const PROGMEM char * const PROGMEM monthNames_P[] =
     monthStr7,monthStr8,monthStr9,monthStr10,monthStr11,monthStr12
 };
 
-const char monthShortNames_P[] PROGMEM = "ErrJanFebMarAprMayJunJulAugSepOctNovDec";
+const char monthShortNames_P[] PROGMEM = "ErrJanFevMarAvrMaiJunJulAouSepOctNovDec";
 
 const char dayStr0[] PROGMEM = "Err";
-const char dayStr1[] PROGMEM = "Sunday";
-const char dayStr2[] PROGMEM = "Monday";
-const char dayStr3[] PROGMEM = "Tuesday";
-const char dayStr4[] PROGMEM = "Wednesday";
-const char dayStr5[] PROGMEM = "Thursday";
-const char dayStr6[] PROGMEM = "Friday";
-const char dayStr7[] PROGMEM = "Saturday";
+const char dayStr1[] PROGMEM = "Dimanche";
+const char dayStr2[] PROGMEM = "Lundi";
+const char dayStr3[] PROGMEM = "Mardi";
+const char dayStr4[] PROGMEM = "Mercredi";
+const char dayStr5[] PROGMEM = "Jeudi";
+const char dayStr6[] PROGMEM = "Vendredi";
+const char dayStr7[] PROGMEM = "Samedi";
 
 const PROGMEM char * const PROGMEM dayNames_P[] =
 {
    dayStr0,dayStr1,dayStr2,dayStr3,dayStr4,dayStr5,dayStr6,dayStr7
 };
 
-const char dayShortNames_P[] PROGMEM = "ErrSunMonTueWedThrFriSat";
+const char dayShortNames_P[] PROGMEM = "ErrDimLunMarMerJeuVenSam";
 
 /* functions to return date strings */
 
